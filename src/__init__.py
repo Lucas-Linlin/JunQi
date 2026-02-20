d6992897fc4471788d8d72f7d391495a3fc44f5b
Lucas-Linlin <zhilin.tang@qq.com>
2026-02-20 11:56:44 +0800

    feat(src): 初始化游戏基本结构和棋子类
    在 `__init__.py` 中添加了棋子等级的字典 `CHESS_LEVEL`，定义了不同等级棋子的名称。
    引入了 `pygame` 库，而不是之前简写的 `pg`。
    新增了 `Chess` 类，继承自 `pygame.sprite.Sprite`，用于表示游戏中的棋子。该类包含棋子等级 `level` 和棋子图片 `image` 的属性，并在初始化时加载相应的图片。
    添加了 `main` 函数，用于初始化游戏窗口并处理主循环事件，包括窗口关闭事件。
    在主程序入口 `if __name__ == "__main__":` 调用 `main` 函数以启动游戏。
    在 `src/images` 文件夹中添加了多个 `.png` 图片文件，用于表示不同等级和阵营的棋子，以及游戏板、移动、隐藏等状态。这些图片文件用于在游戏中显示相应的图形元素。

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,42 @@
 '''
 JunQi game
 '''
-import pygame as pg
+import pygame
+
+CHESS_LEVEL: dict[int, str] = {
+    0: '工兵',
+    1: '排长',
+    2: '连长',
+    3: '营长',
+    4: '团长',
+    5: '旅长',
+    6: '师长',
+    7: '军长',
+    8: '司令',
+    9: '地雷',
+    10: '炸弹',
+    -1: '军旗',
+}
+
+
+class Chess(pygame.sprite.Sprite):
+    def __init__(self, level: int, team:str):
+        self.level: int = level
+        self.image = pygame.image.load(f'{team}-{level}.png')
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((720, 900))
+    pygame.display.set_caption('JunQi')
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
